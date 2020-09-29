a38217e748bca8ab5a4bd86635715dd67f16fb4d
nandikasoi <69952137+nandikasoi@users.noreply.github.com>
2020-09-29 19:12:06 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,71 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+
+        }
+        this.visibility = 255;
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+ 
+      }
+      display(){
+        console.log(this.body.speed);
+        if(this.body.speed < 3){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+      } else{
+           World.remove(world, this.body);
+           push();
+           this.Visiblity = this.Visiblity - 5;
+           pop();
+
+       // set 1
+        block1.score();
+        block2.score();
+        block3.score();
+        block4.score();
+        block5.score();
+        block6.score();
+        block7.score();
+        block8.score();
+        block9.score();
+        block10.score();
+        block11.score();
+        block12.score();
+        block13.score();
+        block14.score();
+        block15.score();
+        block16.score();
+        
+        // set 2
+        blocks1.score();
+        blocks2.score();
+        blocks3.score();
+        blocks4.score();
+        blocks5.score();
+        blocks6.score();
+        blocks7.score();
+        blocks8.score();
+        blocks9.score();
+        
+     } 
+   }
+
+   score () {
+     if(this.Visibility > 0 && this,Visiblity > -105){
+       score++;
+     }
+   }
+}
